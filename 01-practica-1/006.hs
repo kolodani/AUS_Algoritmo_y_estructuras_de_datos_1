8b8d9b529980ebe73f5e71ee80c0cc221255336d
kolodani <danielvinzia@gmail.com>
2023-07-02 14:59:20 -0300

    haskell, ejercicio 006, problema j

--- 01-practica-1/006.hs
+++ 01-practica-1/006.hs
@@ -80,10 +80,15 @@ pares :: [Int] -> [Int]
 pares [] = []
 pares (x : xs) = if mod x 2 == 0 then x : pares (xs) else pares (xs)
 
-{-
-?  j) letras, que dada una lista de caracteres, devuelve la lista de aquellos que son letras
-?     (minúsculas o mayúsculas)
+--  j) letras, que dada una lista de caracteres, devuelve la lista de aquellos que son letras
+--    (minúsculas o mayúsculas)
+letras :: [Char] -> [Char]
+letras [] = []
+letras (x:xs) = if (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') then x : letras (xs) else letras (xs)
 
+
+
+{-
 ?  k) masDe, que dada una lista de listas xss y un número n, devuelve la lista de aquellas listas
 ?     de xss con longitud mayor que n
 -}
