f16fa46d0d827e388076f7ea932cc067276ea79d
kolodani <danielvinzia@gmail.com>
2023-07-02 15:27:39 -0300

    haskell, ejercicio 008, problema a

---
+++ 01-practica-1/008.hs
@@ -0,0 +1,22 @@
+{-
+*    8. Definir las siguientes funciones usando listas por comprensión:
+
+?    a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacı́a
+?       si el entero no es positivo).
+?    b) matches, que dados un entero x y una lista de enteros descarta de la lista los elementos distintos a x.
+?    c) unique, que dada una lista xs de enteros, devuelve la lista con los elementos no repetidos de xs.
+?    d) cuadrupla, que dados cuatro enteros a, b, c y d tales que 0 < a, b, c, d, ≤ 100, devuelve las
+?       cuadruplas (a, b, c, d) que cumplen a2 + b2 = c2 + d2.
+-}
+
+-- a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacı́a
+--    si el entero no es positivo).
+divisors :: Int -> [Int]
+divisors x = if x > 0 then [y | y <- [1..x], x `mod` y == 0] else []
+
+{-
+?    b) matches, que dados un entero x y una lista de enteros descarta de la lista los elementos distintos a x.
+?    c) unique, que dada una lista xs de enteros, devuelve la lista con los elementos no repetidos de xs.
+?    d) cuadrupla, que dados cuatro enteros a, b, c y d tales que 0 < a, b, c, d, ≤ 100, devuelve las
+?    cuadruplas (a, b, c, d) que cumplen a2 + b2 = c2 + d2.
+-}
