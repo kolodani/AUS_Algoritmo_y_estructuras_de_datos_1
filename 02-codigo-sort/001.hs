861fd194eb3931f2ca38d948ad96f83949fa66ae
kolodani <danielvinzia@gmail.com>
2023-07-02 17:20:43 -0300

    haskell, insertion sort

--- 02-codigo-sort/001.hs
+++ 02-codigo-sort/001.hs
@@ -16,8 +16,16 @@ ssort l = m:ssort l'
             m = minimo l
             l' = delete m l
 
+-------------------------------------------------------
+--- Insertion Sort
 
+insert x [] = [x]
+insert x (y:t) = if x < y then (x:y:t)
+                    else y:(insert x t)
 
+isort [] = []
+isort [x] = [x]
+isort (x:t) = insert x (isort t)
 
 
 
