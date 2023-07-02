211ad003ddd433bb26dc29d4dcb867cac1ba1b60
kolodani <danielvinzia@gmail.com>
2023-07-02 17:30:52 -0300

    haskell, Merge Sort msort

--- 02-codigo-sort/001.hs
+++ 02-codigo-sort/001.hs
@@ -65,13 +65,23 @@ split2 (a:b:t) = (a:m, b:n)
                     (m,n) = split2 t
 
 --------------
+
 merge [] [] = []
 merge a [] = a
 merge [] b = b
 merge (x:xs) (y:ys) = if x < y then x:(merge xs (y:ys))
                         else y:(merge (x:xs) ys)
 
+--------------
 
+msort [] = []
+msort [x] = [x]
+msort lista = let
+                    (i, j) = split lista;
+                    i' = msort i;
+                    j' = msort j
+                in
+                    merge i' j'
 
 
 
