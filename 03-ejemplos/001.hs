4752ceaf427abcce481d9bb7f2a553d4a3233e52
kolodani <danielvinzia@gmail.com>
2023-07-03 15:09:28 -0300

    haskell, juntar

--- 03-ejemplos/001.hs
+++ 03-ejemplos/001.hs
@@ -1,2 +1,9 @@
 sumaDivisores d = sum [x | x <- [1..d], mod d x == 0, x /= d]
 perfectos p = [x | x <- [1..p], sumaDivisores x == x]
+
+----------------------------------------------------------
+
+juntar :: (Ord a) => [a] -> [a] -> [a]
+juntar a [] = a
+juntar [] b = b
+juntar (x:xs) (y:ys) = if x <y then (x:juntar xs (y:ys)) else (y:juntar ys (x:xs))
