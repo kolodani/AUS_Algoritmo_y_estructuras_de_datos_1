69407a7327aa8b3459c2be4c7613b69a72c417cf
kolodani <danielvinzia@gmail.com>
2023-07-03 15:13:07 -0300

    haskell, qsort

--- 03-ejemplos/001.hs
+++ 03-ejemplos/001.hs
@@ -7,3 +7,16 @@ juntar :: (Ord a) => [a] -> [a] -> [a]
 juntar a [] = a
 juntar [] b = b
 juntar (x:xs) (y:ys) = if x <y then (x:juntar xs (y:ys)) else (y:juntar ys (x:xs))
+
+----------------------------------------------------------
+
+particion :: (Ord a) => a -> [a] -> ([a],[a])
+particion p [] = ([],[])
+particion p (x:xs) = if x < p then (x:l1,l2) else (l1,x:l2)
+    where (l1,l2) = particion p xs
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort [a] = [a]
+qsort (x:xs) = qsort y ++ [x] ++ qsort ys
+    where (y,ys) = particion x xs
