d11766ba9742aed63fc8ae85577b74a89e0593fd
kolodani <danielvinzia@gmail.com>
2023-07-03 15:28:01 -0300

    haskell, producto escalar

--- 03-ejemplos/001.hs
+++ 03-ejemplos/001.hs
@@ -27,3 +27,10 @@ miZip :: [a] -> [b] -> [(a,b)]
 miZip [] _ = []
 miZip _ [] = []
 miZip (x:xs) (y:ys) = (x,y):miZip xs ys
+
+----------------------------------------------------------
+
+prodEscalar :: [Int] -> [Int] -> Int
+prodEscalar [] _ = 0
+prodEscalar _ [] = 0
+prodEscalar (x:xs) (y:ys) = sum [ x*y | (x,y) <- miZip xs ys]
